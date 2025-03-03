7d4d223ae5227d32e668a8cea1cb942f58d06e5d
Daniel Valério Sampaio <danisampaio@msn.com>
2025-03-03 21:03:56 +0000

    bugfix: host

--- web/config/environments/development.rb
+++ web/config/environments/development.rb
@@ -62,6 +62,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = {
-    host: ENV['CANONICAL_HOST'], port: 3000
+    host: 'localhost', port: 3000
   }
 end
