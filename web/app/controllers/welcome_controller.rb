c2ef0ac027e8eaa752a0477816023bbdf466a3be
Daniel Valério Sampaio <danisampaio@msn.com>
2025-03-03 20:57:58 +0000

    correct address

--- web/app/controllers/welcome_controller.rb
+++ web/app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 class WelcomeController < ApplicationController
   def index
-    @google_maps_url = 'https://goo.gl/maps/nBAxNAsmPSS2'
+    @google_maps_url = 'https://maps.app.goo.gl/CNJj3cWefU9G3WzS6'
     @cool_earth_url = 'https://www.coolearth.org/'
   end
 
