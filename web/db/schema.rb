9fb0d2cc338a4cf9d8e3742f1f7336eef2c56404
Daniel Valério Sampaio <danisampaio@msn.com>
2025-03-23 17:08:13 +0000

    Bug: missing db updates after upgrade

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_06_163153) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_12_220425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -45,9 +45,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_06_163153) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", precision: nil, null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -107,6 +114,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_06_163153) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "plus_ones", "guests"
 
   create_view "attendees", sql_definition: <<-SQL
