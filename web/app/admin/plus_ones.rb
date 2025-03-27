4c784c2ead1609dbac85a018e59c86566cb75574
Daniel Valério Sampaio <danisampaio@msn.com>
2025-03-27 22:35:48 +0000

    Bug: Can't add Plus Ones in Admin Panel

--- web/app/admin/plus_ones.rb
+++ web/app/admin/plus_ones.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register PlusOne do
-  permit_params :first_name, :last_name, :diet, :child
+  permit_params :guest_id, :first_name, :last_name, :diet, :child
 
   form do |_f|
     inputs 'Plus One' do
+      input :guest, as: :select, collection: Guest.pluck(:first_name, :last_name, :id), prompt: "Select a Guest"
       input :first_name
       input :last_name
       input :diet, as: :text
