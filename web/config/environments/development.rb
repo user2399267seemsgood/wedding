c0da2103f75b224276d3618254faa477f5744ab9
Daniel Valério Sampaio <danisampaio@msn.com>
2025-04-05 09:58:15 +0000

    configure fallback locale

--- web/config/environments/development.rb
+++ web/config/environments/development.rb
@@ -40,6 +40,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  # Fallback to English, when entry not found in locale.
+  config.i18n.fallbacks = true
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
--- web/config/initializers/locale.rb
+++ web/config/initializers/locale.rb
@@ -5,5 +5,5 @@ I18n.load_path += Dir[Rails.root.join("config", "locales", "*.{rb,yml}")]
 I18n.available_locales = [:de, :pt, :en]
 
 # Set default locale to something other than :en
-I18n.default_locale = :de
+I18n.default_locale = :en
 
