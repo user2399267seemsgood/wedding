064929514cb0b0e57b789da44afc5e2c28a5a6a9
Daniel Valério Sampaio <danisampaio@msn.com>
2025-05-24 15:32:55 +0000

    fixed sorting, added additional filter, entries all on first page

--- web/app/admin/plus_ones.rb
+++ web/app/admin/plus_ones.rb
@@ -3,6 +3,14 @@
 ActiveAdmin.register PlusOne do
   permit_params :guest_id, :first_name, :last_name, :diet, :child
 
+  # Default sorting by first_name asc, then last_name asc
+  config.sort_order = 'id_dsc'
+  controller do
+    def apply_sorting(chain)
+      params[:order] ? chain : chain.reorder(first_name: :asc, last_name: :asc)
+    end
+  end
+
   form do |_f|
     inputs 'Plus One' do
       input :guest, as: :select, collection: Guest.all.map { |g| ["#{g.first_name} #{g.last_name}", g.id] }, prompt: "Select a Guest"
@@ -13,4 +21,6 @@ ActiveAdmin.register PlusOne do
     end
     actions
   end
+  
+  config.per_page = 500
 end
