b89d83a5191b52a26e0c0995496db7ecea4efe11
Daniel Valério Sampaio <danisampaio@msn.com>
2025-10-31 15:35:35 +0000

    Infos about accommodation and the venue, game suggestions reach out to witnesses, bugfixes about links (open in new tab)

--- web/app/controllers/welcome_controller.rb
+++ web/app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
   def index
     @google_maps_url = 'https://maps.app.goo.gl/wNyS1za5spREvUT4A'
     @google_maps_url_dinner = 'https://maps.app.goo.gl/d6rh3m4TiEZvfFgu5'
+    @google_maps_url_hotel = 'https://maps.app.goo.gl/JNDD6v5TGikUwQaY9'
   end
 
   def switch_locale
